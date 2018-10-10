32b22a8a533ee86e0dece4a7168e4962d39b2d3a
Kristen Shea <kstar61@live.com>
2018-10-10 18:56:39 -0500

    Add project files.

---
+++ CG 14-3 (Timer)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_14_3__Timer_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
